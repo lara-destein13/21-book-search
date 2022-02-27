3a228eee078726a01b5b3e1e94402683a65bbb9b
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-27 17:28:53 -0500

    Start work on typeDefs.json and resolvers with hopes of getting create user working

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+  },
+
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  }
+};
+
+module.exports = resolvers;
